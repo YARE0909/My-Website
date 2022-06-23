4c9be2c373573be08ef30763e29a82f3af6292a1
YARE0909 <94288311+YARE0909@users.noreply.github.com>
2022-06-23 22:11:54 +0530

    Minor Fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,18 @@ module.exports = {
     extend: {
       animation: {
         wiggle: 'wiggle 200ms ease-in-out',
+        fullrotate: 'fullrotate 1000ms ease-in-out',
       },
       keyframes: {
         wiggle: {
           '0%, 100%': { transform: 'rotate(-5deg)' },
           '50%': { transform: 'rotate(5deg)' },
+        },
+        fullrotate: {
+          '0%': { transform: 'rotate(-360deg)' },
+          
         }
-      }
+      },
     },
   },
   plugins: [require('tailwindcss-textshadow')],
